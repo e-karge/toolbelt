4ead476c72e323a0fd3136d37cd32dcdb6753378
Eric Karge <eric.karge@innoq.com>
2021-05-11 21:46:55 +0200

    [buildah] Try to fix build within GitHub workflow /2

--- buildah/install.sh
+++ buildah/install.sh
@@ -7,7 +7,6 @@ adduser -h /buildah -g 'build it!' -s /bin/nologin -k /var/empty buildah -D
 echo buildah:1001:9999 >/etc/subuid
 echo buildah:1001:9999 >/etc/subgid
 
-ls -l /
 install -o buildah -g buildah -m 00700 -d /buildah/.local
 install -o buildah -g buildah -m 00700 -d /buildah/.local/share
 install -o buildah -g buildah -m 00700 -d /buildah/.local/share/containers
