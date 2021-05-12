2f992fa09f6162372bc9c7172c71ab7360ca76e1
Eric Karge <eric.karge@innoq.com>
2021-05-12 14:23:14 +0200

    [syslog-ng] Fix problem with persist file, rename `syslog-ng` volume

--- syslog-ng/install.sh
+++ syslog-ng/install.sh
@@ -1,1 +1,5 @@
 apk add --no-cache syslog-ng syslog-ng-json
+
+adduser -h /run/syslog-ng -g 'log it!' -s /bin/nologin -k /var/empty syslog-ng -D
+
+install -o syslog-ng -g syslog-ng -m 00700 -D -d /run/syslog-ng
