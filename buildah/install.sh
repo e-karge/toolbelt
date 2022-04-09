003619be8e72eaf1b134215f4491e67b3ab17f52
Eric Karge <eric.karge@innoq.com>
2022-04-09 09:53:31 +0200

    [buildah] Fix permissions for `~/.config` directory

--- buildah/install.sh
+++ buildah/install.sh
@@ -7,6 +7,7 @@ adduser -h /buildah -g 'build it!' -s /bin/nologin -k /var/empty buildah -D
 echo buildah:1001:9999 >/etc/subuid
 echo buildah:1001:9999 >/etc/subgid
 
+install -o buildah -g buildah -m 00700 -d /buildah/.config
 install -o buildah -g buildah -m 00700 -d /buildah/.local
 install -o buildah -g buildah -m 00700 -d /buildah/.local/share
 install -o buildah -g buildah -m 00700 -d /buildah/.local/share/containers
