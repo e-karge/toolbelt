3f37daeac22fa6cd1294c7cd38a43b13939ff4c6
Eric Karge <eric.karge@innoq.com>
2024-10-04 23:01:06 +0200

    [podman] Fix ownership of `/podman/.config{,/containers}`

--- podman/install.sh
+++ podman/install.sh
@@ -6,6 +6,8 @@ adduser -h /podman -g 'run it!' -s /bin/nologin -k /var/empty podman -D
 echo podman:1001:9999 >/etc/subuid
 echo podman:1001:9999 >/etc/subgid
 
+install -o podman -g podman -m 00700 -d /podman/.config
+install -o podman -g podman -m 00700 -d /podman/.config/containers
 install -o podman -g podman -m 00700 -d /podman/.local
 install -o podman -g podman -m 00700 -d /podman/.local/share
 install -o podman -g podman -m 00700 -d /podman/.local/share/containers
